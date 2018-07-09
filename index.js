19df0b378e8be8330b60db1ab5ec30cae45cad19
zeroh729 <zeroh729@gmail.com>
2018-07-09 22:00:42 +0800

    Appends data to test spreadsheet in Google Drive

---
+++ index.js
@@ -0,0 +1,119 @@
+const fs = require('fs');
+const readline = require('readline');
+const {google} = require('googleapis');
+var JSSoup = require('jssoup').default;
+var request = require('request');
+
+// If modifying these scopes, delete credentials.json.
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/drive'];
+const TOKEN_PATH = 'credentials.json';
+
+// Load client secrets from a local file.
+fs.readFile('client_secret.json', (err, content) => {
+  if (err) return console.log('Error loading client secret file:', err);
+  // Authorize a client with credentials, then call the Google Sheets API.
+  authorize(JSON.parse(content), appendDada);
+});
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getNewToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getNewToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return callback(err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+
+/**
+ * Prints the names and majors of students in a sample spreadsheet:
+ * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+ */
+function listMajors(auth) {
+  const sheets = google.sheets({version: 'v4', auth});
+  sheets.spreadsheets.values.get({
+    spreadsheetId: '1WuTS-bjEokI9ztfrmpq2i2j3VccNsdeX7fbYqg81NXY',
+    range: 'Sheet1!A:B',
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const rows = res.data.values;
+    if (rows.length) {
+      console.log('Name, Major:');
+      // Print columns A and E, which correspond to indices 0 and 4.
+      rows.map((row) => {
+        console.log(`${row[0]}, ${row[1]}`);
+      });
+    } else {
+      console.log('No data found.');
+    }
+  });
+}
+
+function appendDada(auth) {
+  const sheets = google.sheets({version: 'v4', auth});
+  sheets.spreadsheets.values.append({
+    spreadsheetId: '1WuTS-bjEokI9ztfrmpq2i2j3VccNsdeX7fbYqg81NXY',
+    range: 'Sheet1!A1:B',
+    valueInputOption: 'USER_ENTERED',
+    insertDataOption: 'INSERT_ROWS', 
+    includeValuesInResponse: 'FALSE',
+    responseValueRenderOption: 'FORMATTED_VALUE',
+    resource: {
+      "majorDimension": "ROWS",
+      "range": "Sheet1!A1:B",
+      "values": [
+        [
+          "Hello!", "World!"
+        ]
+      ]
+    }
+  }, (err, res) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    console.log(res.status == 200 ? "Success" : "Status not 200");
+  });
+}
